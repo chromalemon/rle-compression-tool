aa735414c473a419f77601c05523ff8004aafe02
chromalemon <nehon9914@gmai.com>
2026-07-05 15:47:40 +0100

    Vim auto-save: Sun Jul  5 15:47:40 BST 2026

--- src/calc.c
+++ src/calc.c
@@ -3,18 +3,18 @@
 long calc_file_size(const char* path){
 	FILE* fp = fopen(path, "rb");
 	if (!fp){
-		perror("Could not open file to calculate size.\n");
+		fprintf(stderr, "Error: Could not open file to calculate size.\n");
 		return -1L;
 	}
 	
 	if (fseek(fp, 0L, SEEK_END) != 0){
-		perror("Fseek failed.\n");
+		fprintf(stderr, "Error: Fseek failed.\n");
 		fclose(fp);
 		return -1L;
 	}
 	long size = ftell(fp);
 	if (size == -1L){
-		perror("Ftell failed.\n");
+		fprintf(stderr, "Error: Ftell failed.\n");
 	}
 
 	fclose(fp);
@@ -26,7 +26,6 @@ void print_diff(const char* input_path, const char* output_path){
 	long original = calc_file_size(input_path);
 	long new = calc_file_size(output_path);
 	if (original == -1L || new == -1L){
-		perror("Could not calculate file sizes.\n");
 		return;
 	}
 
