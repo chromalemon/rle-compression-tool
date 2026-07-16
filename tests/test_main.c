ad569eff1be1cdafd04386913b7350d9e8608827
chromalemon <nehon9914@gmai.com>
2026-07-16 23:17:32 +0100

    Vim auto-save: Thu Jul 16 23:17:32 BST 2026

--- tests/test_main.c
+++ tests/test_main.c
@@ -2,6 +2,9 @@
 
 void test_single_character_compression(void);
 void test_single_character_decompression(void);
+void test_no_repeat_compression(void);
+void test_no_repeat_decompression(void);
+
 
 int main(void)
 {
@@ -9,6 +12,8 @@ int main(void)
 
     RUN_TEST(test_single_character_compression);
     RUN_TEST(test_single_character_decompression);
+    RUN_TEST(test_no_repeat_compression);
+    RUN_TEST(test_no_repeat_decompression);
 
     return UNITY_END();
 }
--- tests/test_rle.c
+++ tests/test_rle.c
@@ -24,8 +24,8 @@ void test_single_character_compression(void)
     res = compress(&mem);
     TEST_ASSERT_EQUAL_INT(1, res);
 
-    TEST_ASSERT_EQUAL_UINT8(1, mem.out_buf[0]);
-    TEST_ASSERT_EQUAL_UINT8('A', mem.out_buf[1]);
+    unsigned char expected[2] = {1, 'A'};
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected, mem.out_buf, 2);
     
     goto cleanup;
 
@@ -48,6 +48,52 @@ void test_single_character_decompression(void)
 
     TEST_ASSERT_EQUAL_UINT8('A', mem.out_buf[0]);
 
+    goto cleanup;
+
+cleanup:
+    free(mem.inp_buf);
+    free(mem.out_buf);
+
+}
+
+void test_no_repeat_compression(void)
+{
+    mem_struct mem;
+    int res = mem_init(&mem, 1, 0, 1, 0, 6);
+    TEST_ASSERT_EQUAL_INT(1, res);
+
+    unsigned char str[6] = {'A', 'B', 'C', 'D', 'E', 'F'};
+    memcpy(mem.inp_buf, str, 6);
+
+    res = compress(&mem);
+    TEST_ASSERT_EQUAL_INT(1, res);
+
+    unsigned char expected[12] = {1, 'A', 1, 'B', 1, 'C', 1, 'D', 1, 'E', 1, 'F'};
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected, mem.out_buf, 12);
+    
+    goto cleanup;
+
+cleanup:
+    free(mem.inp_buf);
+    free(mem.out_buf);
+
+}
+
+void test_no_repeat_decompression(void)
+{
+    mem_struct mem;
+    int res = mem_init(&mem, 1, 0, 0, 6, 12);
+    TEST_ASSERT_EQUAL_INT(1, res);
+
+    unsigned char str[12] = {1, 'A', 1, 'B', 1, 'C', 1, 'D', 1, 'E', 1, 'F'};
+    memcpy(mem.inp_buf, str, 12);
+
+    res = decompress(&mem);
+    TEST_ASSERT_EQUAL_INT(1, res);
+
+    unsigned char expected[6] = {'A', 'B', 'C', 'D', 'E', 'F'};
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected, mem.out_buf, 6);
+    
     goto cleanup;
 
 cleanup:
